a6442f9ca9d6bde643494e90d8ac46c3c589ce79
PauloSalvatierra <paulosalvatierra@hotmail.com>
2023-09-13 21:51:08 -0300

    PreEntrega+SalvatierraFrias

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,14 @@
+import cart from './assets/cart.svg'
+const CartWidget =() => {
+return(
+    <div>
+        <img src ={cart} alt="imagen de carrito"/>
+        0
+    </div>
+
+)
+
+
+
+}
+export default CartWidget
