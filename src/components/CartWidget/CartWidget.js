7a0dcd5173202e4166443d5e360aaeaaf3e86166
PauloSalvatierra <paulosalvatierra@hotmail.com>
2023-10-28 14:53:40 -0300

    change

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,11 +1,14 @@
 import cart from './assets/cart.svg'
+import { useContext } from 'react'
+import { CartContext } from '../../context/CartContext'
+import { Link } from 'react-router-dom'
 const CartWidget =() => {
+    const {totalQuantity} = useContext(CartContext)
 return(
-    <div>
-        <img src ={cart} alt="imagen de carrito"/>
-        0
-    </div>
-
+    <Link to='/cart' className='CartWidget' style={{ dispaly: totalQuantity > 0 ? 'block' : 'none'}}>
+        <img className='CartImg' src={cart} alt='cart-widget'/>
+        {totalQuantity}
+        </Link>
 )
 
 
